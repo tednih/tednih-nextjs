440b1319ce499585ed597f26bc1c0ce7eba20315
tednih <122004183+tednih@users.noreply.github.com>
2023-03-08 11:17:40 +0800

    fix detail project page

--- app/projects/[id]/page.js
+++ app/projects/[id]/page.js
@@ -1,5 +1,7 @@
 "use client";
+import Footer from "@/app/components/footer";
 import Navbar from "@/app/components/navbar";
+import Image from "next/image";
 import React from "react";
 import { Projects } from "../dataProjects";
 
@@ -14,28 +16,26 @@ const DetailProjects = ({ params }) => {
   return (
     <div>
       <Navbar />
-      <section className="overflow-hidden bg-zinc-200 dark:bg-zinc-800 sm:grid sm:grid-cols-2 sm:items-center">
-        <div className="p-8 md:p-12 lg:px-16 lg:py-24">
+      <section className="lg:max-w-[1440px] m-auto overflow-hidden bg-zinc-100 dark:bg-zinc-900 sm:grid sm:grid-cols-2 sm:items-center">
+        <div className="p-8 md:p-12 lg:px-16 lg:py-14">
           <div className="mx-auto max-w-xl text-center sm:text-left">
             <h2 className="text-2xl font-bold text-gray-900 dark:text-white md:text-3xl">
               {detail.judul}
             </h2>
 
-            <p className="hidden text-gray-500 dark:text-gray-400 md:mt-4 md:block">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Et,
-              egestas tempus tellus etiam sed. Quam a scelerisque amet
-              ullamcorper eu enim et fermentum, augue. Aliquet amet volutpat
-              quisque ut interdum tincidunt duis.
+            <p className="text-gray-500 dark:text-gray-400 md:mt-4 ">
+              {detail.deskripsi}
             </p>
           </div>
         </div>
 
-        <img
+        <Image
           alt=""
           src={detail.foto}
-          className="h-full w-full object-cover sm:h-[calc(100%_-_2rem)] sm:self-end sm:rounded-tl-[30px] md:h-[calc(100%_-_4rem)] md:rounded-tl-[60px]"
+          className="h-full w-full object-fit rounded-md"
         />
       </section>
+      <Footer />
     </div>
   );
 };
--- app/projects/dataProjects.js
+++ app/projects/dataProjects.js
@@ -1,31 +1,91 @@
 import Foto1 from "../../public/luxspace.png";
-import Foto2 from "../../public/portfolio1.png";
 import Foto3 from "../../public/portfolio2.jpg";
+import React from "react";
+import Image from "next/image";
+
+const linkName = "Build With Angga";
+const link = (
+  <>
+    <a
+      className="underline"
+      href="https://class.buildwithangga.com/course_playing/master-class-react-js-dan-tailwind-css-website-development/"
+      target="_blank"
+    >
+      {linkName}
+    </a>
+  </>
+);
+const linkLuxspace = "Luxspace";
+const luxspace = (
+  <>
+    <a
+      className="underline"
+      href="https://luxspace-ted.vercel.app/"
+      target="_blank"
+    >
+      {linkLuxspace}
+    </a>
+  </>
+);
+const linkPustaka = "Pustaka Booking";
+const pustaka = (
+  <>
+    <a
+      className="underline"
+      href="https://github.com/tednih/pustaka-booking"
+      target="_blank"
+    >
+      {linkPustaka}
+    </a>
+  </>
+);
+
+const sertifikatBwa = (
+  <>
+    <Image
+      src="/sertifikat-BWA.jpg"
+      alt="Sertifikat BWA"
+      width={300}
+      height={300}
+    />
+  </>
+);
 
 const Projects = [
   {
     id: 1,
     judul: "Luxspace",
-    tanggal: "20 Januari 2023",
+    tanggal: "13 Februari 2023",
     foto: Foto1,
     bahasa: ["HTML", "CSS", "JavaScript"],
     tools: ["React", "Tailwind"],
+    deskripsi: (
+      <>
+        Project ini dibuat saat saya mengikuti kelas online dari {link}. Di
+        project ini saya mempelajari menggunakan ReactJs dan Tailwind serta
+        bagaimana membuat skeleton loading, hooks, router, dan lainnya didalam
+        reactJs. Lihat Project : {luxspace}. Hasil dari Kelas ini :{" "}
+        {sertifikatBwa}
+      </>
+    ),
   },
+
   {
     id: 2,
-    judul: "Portfolio pertama",
-    tanggal: "23 Januari 2023",
-    foto: Foto2,
-    bahasa: ["HTML", "CSS", "JavaScript"],
-    tools: [],
-  },
-  {
-    id: 3,
-    judul: "CRUD CodeIgniter",
+    judul: "Pustaka Booking",
     tanggal: "28 Mei 2019",
     foto: Foto3,
     bahasa: ["PHP"],
     tools: ["CodeIgniter"],
+    deskripsi: (
+      <>
+        Aplikasi Web dengan Framework Codeigniter. Ini adalah aplikasi web yang
+        dibuat berdasarkan modul materi Web Programming II Universitas Bina
+        Sarana Informatika, dengan menggunakan framework Codeigniter 3. di
+        project ini saya mempelajari bahasa pemrograman PHP dan cara CRUD
+        menggunakan framework CodeIgniter 3. Lihat sourcecode : {pustaka}
+      </>
+    ),
   },
 ];
 
