3b3efd0a981302bcf40a1ee446706e2a0e7dfc55
tednih <dimassted@gmail.com>
2023-04-18 17:30:07 +0700

    ok

--- app/projects/page.js
+++ app/projects/page.js
@@ -2,10 +2,6 @@
 import React from "react";
 import CardProjects from "./cardProjects";
 
-export const metadata = {
-  title: 'Projects',
-}
-
 const Projects = () => {
   return (
     <div className="bg bg-zinc-100 dark:bg-zinc-900 py-10 lg:h-screen">
